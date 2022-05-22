8c8df921afd063aae3db66053e642b6b9ce1c243
Roman_Piliugin <roman.pilyugin@gmail.com>
2022-05-22 11:09:20 +0300

    Solve hovework-3

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,18 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+	commonSize := len(input)
+	keys := make([]int, 0, commonSize)
+	for k := range input {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	output := make([]string, 0, commonSize)
+	for _, val := range keys {
+		output = append(output, input[val])
+	}
+	return output
 }
